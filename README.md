5902a4ca48d1f39d779baf300dc1ee12958dcdf4
Alonso Cornejo <acornejo1605@gmail.com>
2025-08-26 12:52:33 -0400

    Ready to Fork

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# ExtraCredit1Module2
+# Assignment: Business Feedback Form
+
+In this exercise, you will practice working with DOM methods and event handling in JavaScript.  
+
+You will complete the provided **`script.js`** and **`index.html`** file by following the **TODO comments**.  
+
+
+
+## Learning Goals
+- Select elements using **`querySelector`**
+- Explore DOM hierarchy with **`parentElement`** and **`children`**
+- Add event listeners with **`addEventListener`**
+- Use **conditional operators (`? :`)** to build dynamic messages
+- Debug values with **`console.log`**
+
+
+
+## Instructions
+1. Open `index.html` in your browser.  
+2. Complete the missing parts in `index.html` and `script.js` where the `TODO` comments are written.  
+3. Use the browser **console** to check your logs.  
+4. When you submit the form, an **alert** should display a personalized message.  
+
+
+
+## Expected Output Examples
+
+### Console Logs
+![alt text](./assets/console.png)
+
+### Alert Messages
+![alt text](./assets/Positive.png)  
+*Positive feedback of 4 or More*
+
+![alt text](./assets/Negative.png)  
+*Negative feedback of less of 4*
+
+
+
+
---
+++ script.js
@@ -0,0 +1,33 @@
+// Select the form using querySelector
+const form="Your code instead of this string";
+
+// TODO: Attach a "submit" event listener to the form
+//Start of listener
+  // TODO: Prevent the form from refreshing the page
+  
+  /* 1. Show parent/children Data for learning/debugging */
+  
+  // TODO: Log the form's children collection in the console
+  // TODO: Log the form's parent element tag name
+  // TODO: Log how many children the form has
+
+  /* 2. Extract values */
+  // TODO: Select the name input
+  const nameInput = "Your code instead of this string";
+
+  // TODO: Select the satisfaction input by its id
+  const satisfactionInput = "Your code instead of this string";
+
+  //Get the value from the name input and trim spaces
+  const name = nameInput.value.trim();
+  //Convert satisfaction input to a numbe
+  const score = parseInt(satisfactionInput.value);
+
+  /* 3. TODO: Conditional operator
+    Use the conditional operator ( ? : ) to create a message INCLUDING NAME INPUT where
+    If score >= 4 → thank the customer
+    Otherwise → promise to improve
+  */
+
+  // 4. TODO: Show the message in an alert
+//End of listener
